88515f4fef420d51ac8621ce4c41519d1a1f9e5b
Malxi <1421215708@qq.com>
2018-12-18 17:16:39 +0800

    add more instructions

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 A python script to create testcase for MIPS CPU
 # Usage
 run insGenerator.py
+Modify the newList(write reg) and useList(read reg) in main to generate testcase for the instruction set.
 
--- insType.py
+++ insType.py
@@ -10,6 +10,12 @@ cal_i = {"use":{'rs':1},
                }
         }
 
+cal_s = {"use":{'rt':1},
+         "new":{'E':{'rd':1},
+                'M':{'rd':0}
+               }
+        }
+
 load =  {"use":{'rs':1},
          "new":{'E':{'rt':2},
                 'M':{'rt':2}
@@ -31,3 +37,21 @@ link = {"use":{},
                'M':{'$ra':0}}}
 j_o = {"use":{},
      "new":{}}
+j_r_link = {"use":{'rs':0},
+        "new":{'E':{'rd':0},
+               'M':{'rd':0}}}
+mf_type = {"use":{},
+        "new":{'E':{'rd':0},
+               'M':{'rd':0}}}
+mt_type = {"use":{'rs':0},
+        "new":{'E':{'hi':0},
+               'M':{'hi':0}}}
+md_type = {"use":{'rs':1, 'rt':1},
+        "new":{'E':{'hi':1, 'lo':1},
+               'M':{'hi':0, 'lo':0}}}
+mfc0 = {"use":{},
+        "new":{'E':{'rt':0},
+               'M':{'rt':0}}}
+mtc0 = {"use":{'rt':0},
+        "new":{'E':{'cp0':0},
+               'M':{'cp0':0}}}
